51598650d8df3199e93c9774c97e5f73f369fa97
Coding <codingmaniac227@gmail.com>
2025-08-30 18:45:32 -0400

    feat(die): create Die component with value prop

---
+++ src/components/Die.jsx
@@ -0,0 +1,14 @@
+import './Die.css'
+
+export default function Die(props) {
+    return (
+        <>
+            <button
+                onClick={''}
+                type="button"
+            > 
+                {props.value}
+            </button>
+        </>
+    )
+}
