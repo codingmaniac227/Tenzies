a1d0164f78ca95968ec2a09553d387e7abd7bf42
Coding <codingmaniac227@gmail.com>
2025-08-30 19:56:32 -0400

    refactor(grid): move dice mapping logic into DiceGrid component

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,25 @@ import DiceGrid from './components/DiceGrid'
 import Die from './components/Die'
 
 
+ function getRandomDieValue() {
+    return Math.floor(Math.random() * 6) + 1
+}
+
 export default function App() {
+
+  const dice = Array.from({ length: 10 }, () => getRandomDieValue())
+
   return (
     <>
       <main className="tenzies-background">
         <article className="tenzies-background-canvas">
-          
+
           <DiceGrid className='dice-grid'>
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
-            <Die value={Math.floor(Math.random() * 6) + 1} />
+            {dice.map((value, i) => (
+              <Die key={i} value={value} />
+            ))}
           </DiceGrid>
+          
         </article>
       </main>
     </>
