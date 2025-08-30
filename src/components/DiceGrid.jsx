e190791047e1c084602eb24cb83d8f8a9cb6eb85
Coding <codingmaniac227@gmail.com>
2025-08-30 18:44:25 -0400

    feat(grid): add DiceGrid component for grouping dice

---
+++ src/components/DiceGrid.jsx
@@ -0,0 +1,10 @@
+import '../App.css'
+
+
+export default function DiceGrid({className, children}) {
+    return (
+        <>
+            <div className={className} role='group' aria-label='Dice'>{children}</div>
+        </>
+    )
+}
