86a58e4bf54027945d87e83621ed4976284a331d
Coding <codingmaniac227@gmail.com>
2025-08-30 15:39:59 -0400

    chore(init): initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <>
+      <main className="tenzies-background">
+        <article className="tenzies-background-canvas">
+          
+        </article>
+      </main>
+    </>
+  )
+}
