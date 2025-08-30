e20ea14cc3ffe2c6925dbc1416fef2659122e817
Coding <codingmaniac227@gmail.com>
2025-08-30 16:18:16 -0400

    feat(app): implement main and article structure for tenzies board

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import './App.css'
+
 export default function App() {
   return (
     <>
       <main className="tenzies-background">
         <article className="tenzies-background-canvas">
-          
+
         </article>
       </main>
     </>
