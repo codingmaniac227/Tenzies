b3c7060e1db1a79f46bd286a42b1bb3865a2a4db
Coding <codingmaniac227@gmail.com>
2025-08-30 18:47:44 -0400

    refactor(app): integrate DiceGrid, Die, and Header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
+import DiceGrid from './components/DiceGrid'
+import Die from './components/Die'
+
 
 export default function App() {
   return (
     <>
       <main className="tenzies-background">
         <article className="tenzies-background-canvas">
-
+          
+          <DiceGrid className='dice-grid'>
+            <Die value={Math.floor(Math.random() * 6) + 1} />
+            <Die value={Math.floor(Math.random() * 6) + 1} />
+            <Die value={Math.floor(Math.random() * 6) + 1} />
+            <Die value={Math.floor(Math.random() * 6) + 1} />
+            <Die value={Math.floor(Math.random() * 6) + 1} />
+            <Die value={Math.floor(Math.random() * 6) + 1} />
+            <Die value={Math.floor(Math.random() * 6) + 1} />
+            <Die value={Math.floor(Math.random() * 6) + 1} />
+            <Die value={Math.floor(Math.random() * 6) + 1} />
+            <Die value={Math.floor(Math.random() * 6) + 1} />
+          </DiceGrid>
         </article>
       </main>
     </>
