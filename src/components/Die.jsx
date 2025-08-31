abd8dbc6310d4d3b30e79197b1432d3098451ab9
Coding <codingmaniac227@gmail.com>
2025-08-31 01:03:07 -0400

    feat(Die): add isHeld visual toggle with conditional class and wire up saveDie onClick

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,11 +1,14 @@
 import './Die.css'
 
 export default function Die(props) {
+
+
     return (
         <>
             <button
-                onClick={''}
+                onClick={() => props.saveDie(props.id)}
                 type="button"
+                className= {props.isHeld ? 'button matched' : 'button'}
             > 
                 {props.value}
             </button>
