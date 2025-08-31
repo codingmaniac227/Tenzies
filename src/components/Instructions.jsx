2150dc6e676be02dbd2397b383cd159ec4f75664
Coding <codingmaniac227@gmail.com>
2025-08-31 01:51:16 -0400

    feat(init): initialized instructions section for Tenzies

---
+++ src/components/Instructions.jsx
@@ -0,0 +1,10 @@
+export default function Instructions(props) {
+    return (
+        <>
+            <section className={props.className}>
+                <h1>Tenzies</h1>
+                <h2>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h2>
+            </section>
+        </>
+    )
+}
