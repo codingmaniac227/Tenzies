4ab660f5347acd70cd31bd9c5382315c7be7545b
Coding <codingmaniac227@gmail.com>
2025-08-31 05:46:38 -0400

    feat(app): implement win screen, new game reset, and roll logic updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { nanoid } from 'nanoid'
 import DiceGrid from './components/DiceGrid'
 import Die from './components/Die'
 import Instructions from './components/Instructions'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 
  function getRandomDieValue() {
@@ -21,7 +21,19 @@ function generateAllNewDice() {
 export default function App() {
   
   const [dice, setDice] = useState(() => generateAllNewDice())
+  const buttonRef = useRef(null)
+  
+  const hasWon = 
+    dice.length > 0 &&
+    dice.every(d => d.isHeld) &&
+    dice.every(d => d.value === dice[0].value)
 
+
+  useEffect(() => {
+    if (hasWon) {
+      buttonRef.current.focus()
+    }
+  }, [hasWon])
 
   function rollDice() {
     setDice(prev => 
@@ -38,22 +50,41 @@ export default function App() {
       console.log(id)
   }
   
+  function newGame() {
+    setDice(generateAllNewDice())
+  }
+
 
   return (
     <>
+      { hasWon ? (
+        <main className="tenzies-background-win">
+        <article className="tenzies-background-canvas-win">
+          <section className='tenzies-instructions-win'>
+              <h1>YOU WON!</h1>
+          </section>
+          <button ref={buttonRef} type='button' onClick={newGame} className='new-game'>
+              New Game
+          </button>
+        </article>
+      </main>
+      ) : (
       <main className="tenzies-background">
         <article className="tenzies-background-canvas">
-          <Instructions className='tenzies-instructions' />
+          <Instructions className='tenzies-instructions' hasWon={hasWon} />
           <DiceGrid className='dice-grid'>
             {dice.map(die => (
               <Die key={die.id} id={die.id} value={die.value} isHeld={die.isHeld} saveDie={saveDie} />
             ))}
           </DiceGrid>
+
           <button type='button' onClick={rollDice} className='roll-btn'>
             Roll
           </button>
+
         </article>
       </main>
+      )}
     </>
   )
 }
