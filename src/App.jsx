0df09f49455e9e85d64c680f8300c289a279ee3d
Coding <codingmaniac227@gmail.com>
2025-08-30 21:00:27 -0400

    refactor(): fix dice state initialization and mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
 import './App.css'
 import DiceGrid from './components/DiceGrid'
 import Die from './components/Die'
+import { useEffect, useState } from 'react'
 
 
  function getRandomDieValue() {
-    return Math.floor(Math.random() * 6) + 1
+  return Math.floor(Math.random() * 6) + 1
+}
+
+function generateAllNewDice() {
+  return Array.from({ length: 10 }, () => getRandomDieValue())
 }
 
 export default function App() {
+  
+  const [die, setDie] = useState(() => generateAllNewDice())
 
-  const dice = Array.from({ length: 10 }, () => getRandomDieValue())
+  function rollDice() {
+    setDie(generateAllNewDice)
+    console.log(die)
+  }
+  
 
   return (
     <>
       <main className="tenzies-background">
         <article className="tenzies-background-canvas">
-
           <DiceGrid className='dice-grid'>
-            {dice.map((value, i) => (
+            {die.map((value, i) => (
               <Die key={i} value={value} />
             ))}
           </DiceGrid>
-          
+          <button type='button' onClick={rollDice} className='roll-btn'>
+            Roll
+          </button>
         </article>
       </main>
     </>
